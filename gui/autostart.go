d1692542d8f8d75393f5069f5a326558a626ca90
AndrefHub <andrew02541632@gmail.com>
2024-01-27 17:13:17 +0300

    rewrote autostart code to symlinks instead on registry entries

--- gui/autostart.go
+++ gui/autostart.go
@@ -3,30 +3,25 @@ package gui
 import (
 	"log"
 	"os"
-	"os/exec"
-	"strings"
+	"path/filepath"
 )
+
+func GetSymlinkPath() string {
+	// Get user's home dir to enable autostart
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filename := filepath.Base(os.Args[0])
+
+	return homeDir + `\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\` + filename
+}
 
 // CheckAutostart checks if the application is already added to autostart
 func CheckAutostart() bool {
 	// Get the path to the current executable
-	exePath, err := os.Executable()
-	if err != nil {
-		log.Println("Error getting executable path:", err)
-		return false
-	}
-
-	// Check the registry for the autostart entry
-	key := `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run`
-	cmd := exec.Command("reg", "query", key)
-	output, err := cmd.Output()
-	if err != nil {
-		log.Println("Error querying registry:", err)
-		return false
-	}
-
-	// Check if the executable path exists in the registry output
-	return strings.Contains(string(output), exePath)
+	_, err := os.Stat(GetSymlinkPath())
+	return !os.IsNotExist(err)
 }
 
 // AddAutostart adds the application to autostart
@@ -38,55 +33,26 @@ func AddAutostart() {
 		return
 	}
 
-	// Add registry key to enable autostart
-	key := `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run`
-	err = os.Setenv("KEY", key)
+	// Get through all symlinks to get real executable path
+	realPath, err := filepath.EvalSymlinks(exePath)
 	if err != nil {
-		log.Println("Error setting environment variable:", err)
+		log.Println("Error getting real executable path:", err)
 		return
 	}
 
-	// Set the value to the path of the executable
-	err = os.Setenv("VALUE", exePath)
-	if err != nil {
-		log.Println("Error setting environment variable:", err)
-		return
-	}
-
-	// Run the PowerShell script to add the registry entry
-	cmd := exec.Command("powershell", "-Command", "(Get-ItemEnv:$env:KEY).SetValue((Get-ItemEnv:$env:KEY).GetValueNames()[0], $env:VALUE)")
-	if err := cmd.Run(); err != nil {
-		log.Println("Error adding registry entry:", err)
-		return
-	}
-
+	os.Symlink(realPath, GetSymlinkPath())
 	log.Println("Autostart entry added successfully!")
 }
 
 // RemoveAutostart removes the application from autostart
 func RemoveAutostart() {
-	// // Get the path to the current executable
-	// exePath, err := os.Executable()
-	// if err != nil {
-	// 	log.Println("Error getting executable path:", err)
-	// 	return
-	// }
-
-	// Remove the registry key to disable autostart
-	key := `HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run`
-	err := os.Setenv("KEY", key)
+	// Remove the symlink to executable to disable autostart
+	err := os.Remove(GetSymlinkPath())
 	if err != nil {
-		log.Println("Error setting environment variable:", err)
+		log.Println("Error deleting old symlink:", err)
 		return
 	}
-
-	// Run the PowerShell script to remove the registry entry
-	cmd := exec.Command("powershell", "-Command", "(Get-ItemEnv:$env:KEY).DeleteValue((Get-ItemEnv:$env:KEY).GetValueNames()[0])")
-	if err := cmd.Run(); err != nil {
-		log.Println("Error removing registry entry:", err)
-		return
-	}
-
+	
 	log.Println("Autostart entry removed successfully!")
 }
 
